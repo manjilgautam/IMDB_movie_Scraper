798379b1bd31e88c7fd586e971f27b04fab409d6
manjil01 <gautamamanjil@gmail.com>
2020-05-31 16:31:13 -0400

    :tada: :sparkles: Back-end finished

---
+++ server/clojure.js
@@ -0,0 +1,12 @@
+const directors = [];
+
+const tester = [];
+
+
+
+function getItems(temp_array) {
+    return tester.forEach(function(i, element) {
+        const dirt = 'Hello'; // do some weird stuff here
+        temp_array.push(dirt)
+    });
+}
--- server/scrapper.js
+++ server/scrapper.js
@@ -9,8 +9,14 @@ const movieURL = 'https://www.imdb.com/title/';
 // const $ = cherio.load(tex);
 // console.log($('#fruits').find('li').length);
 
+const movieCache = {};
+const SearchCache = {};
 
 function searchMovies(searchTerm) {
+    if(SearchCache[searchTerm]) {
+        console.log('Serving from cache...', searchTerm);
+        return Promise.resolve(SearchCache[searchTerm]);
+    }
     return fetch(`${searchURL}${searchTerm}`)
         .then(response => response.text())
         .then(body => {
@@ -31,28 +37,91 @@ function searchMovies(searchTerm) {
                     movies.push(movie);
                 }
             });
-
+            SearchCache[searchTerm] = movies;
             return movies;
         });
 }
 
 function getMovies(imdbId) {
+
+    if(movieCache[imdbId]) {
+        console.log('Serving from cache...', imdbId);
+        return Promise.resolve(movieCache[imdbId]);
+    }
+
     return fetch(`${movieURL}${imdbId}`)
         .then(response => response.text())
         .then(body => {
             const $ = cherio.load(body);
             const $title = $('.title_wrapper h1');
-            const $rating = $('.title_wrapper').find('.subtext');
+            const rating = $('div.subtext').children().remove().end().text().replace(/\n/g, '').replace(/,/g,'').trim();
+            const time = $('time').text();
+            const imdbRating = $('span[itemProp="ratingValue"]').text();
+            const poster = $('div.poster a img').attr('src')
+            const genre = ['Dummy', 'Dummy', "Dummy"];
+            const datePublished = "00-00-0000";
+            const summary = $('div.summary_text').text().trim();
+            const crews = $('div.credit_summary_item').text().trim().replace(/\n/g, '').replace(/,/g,'').trim();
+            const _crews = crews.substring(0, crews.length - 34);
+
+            const rawStory = $('#titleStoryLine div').text().trim();
+
+            const index_of = rawStory.indexOf('\n');
+            const storyLine =  rawStory.substring(0,index_of);
+
+            const company = "Progress...";
+
+            const trailer_link = $('div.slate a').attr('href');
             
-            console.log($rating.text());
+            
+            
+
+
+            // const dirs = []
+
+            // $('div.credit_summary_item').each(function(i, element) {
+            //     const director = $(element).text().trim();
+
+            //     console.log('The first one is: ', director);
+            
+            // });
+
+
+           // const content = $('.subtext a').text();
+           // console.log('The content is: ', content);
+
+        //    const sumary = $('div.credit_summary_item a').text();
+        //    console.log(sumary);
+        //     const crewMember = []
+        //    $('div.credit_summary_item a').each(function(i, element) {
+        //        const director = $(element).text().trim();
+        //        console.log('This is a ', director);
+        //    })
+           
+
+
             
             const title = $title.first().contents().filter(function() {
                 return this.type === 'text';
             }).text().trim();
 
-            return {
+            const movie = {
+                imdbId,
                 title,
+                rating,
+                time,
+                genre,
+                datePublished,
+                imdbRating,
+                poster,
+                summary,
+                _crews,
+                storyLine,
+                company,
+                trailer_link: `https://www.imdb.com/${trailer_link}` 
             };
+            movieCache[imdbId] = movie;
+            return movie;
         });
 };
 
